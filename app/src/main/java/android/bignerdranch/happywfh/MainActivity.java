ff0ac0ff4f760f314f464418cb944cfe1acdfeeb
Tim Kao <pakuya.kao@gmail.com>
2020-03-22 12:10:51 -0700

    getting the iBinder from remote service and converting it to local ICommon

--- app/src/main/java/android/bignerdranch/happywfh/MainActivity.java
+++ app/src/main/java/android/bignerdranch/happywfh/MainActivity.java
@@ -7,14 +7,18 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private android.bignerdranch.happywfh.ICommon common;
+
     private ServiceConnection serviceCon = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-
+            common = android.bignerdranch.happywfh.ICommon.Stub.asInterface(iBinder);
         }
 
         @Override
@@ -35,12 +39,16 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent("");
                 bindService(intent, serviceCon, BIND_AUTO_CREATE);
             }
-        });
+        }); 
 
         findViewById(R.id.btnCalculate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                try {
+                    Log.i("@tim", "Addition is - " + common.calculate(100, 10));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
