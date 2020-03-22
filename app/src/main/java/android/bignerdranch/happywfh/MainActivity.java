3af5ba4c97735b0bc03ab8d2cb49645df5f9812c
Tim Kao <pakuya.kao@gmail.com>
2020-03-22 12:03:05 -0700

    added service connection skeleton and set listeners

--- app/src/main/java/android/bignerdranch/happywfh/MainActivity.java
+++ app/src/main/java/android/bignerdranch/happywfh/MainActivity.java
@@ -2,13 +2,46 @@ package android.bignerdranch.happywfh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ServiceConnection serviceCon = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnBind).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent("");
+                bindService(intent, serviceCon, BIND_AUTO_CREATE);
+            }
+        });
+
+        findViewById(R.id.btnCalculate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
